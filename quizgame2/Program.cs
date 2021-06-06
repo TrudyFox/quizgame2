1c3130875e48cb4cbd7d5883a8f5b8b417ad2624
TrudyFox <Trudy.Mk@outlook.com>
2021-06-06 12:59:23 -0700

    implementaion with fixed correct answer

--- quizgame2/Program.cs
+++ quizgame2/Program.cs
@@ -10,27 +10,30 @@ namespace quizgame2
             //ask question
 
             string question;
-            question = "how old is oprah";
+            question = "how many legs does oprah have?";
             Console.Write(question);
 
             //put out 4 mutltipul choice answers
             List<string> answers = new List<string>();
-            answers.Add("fifty");
-            answers.Add("twenty");
-            answers.Add("thirtythree");
+            answers.Add("one");
+            answers.Add("three");
+            answers.Add("thirty");
             answers.Add("eighty");
 
             foreach (string answer in answers)
+            {
                 Console.WriteLine(answer);
+            }
 
             //read user input
             string userinput = Console.ReadLine();
-            if (userinput == "twenty")
+
+            //compair user intput to answers the answer number 3 is always the correct answer
+            if (userinput == answers[2])
                 Console.WriteLine("yay");
             else
                 Console.WriteLine("bummer");
 
-            //compair user intput to answers
 
 
 
